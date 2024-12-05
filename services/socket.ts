ef485accb55d220879abc3aac95fd439332dbf1f
preciousaffiah <preciousaffiah205@gmail.com>
2024-12-05 17:53:38 +0100

    updated base url

--- services/socket.ts
+++ services/socket.ts
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3001", {
-// const socket = io("https://youchatbackend-kga1.onrender.com", {
+// const socket = io("http://localhost:3001", {
+const socket = io("https://youchatbackend-kga1.onrender.com", {
   //   transports: ["websocket"],
 });
 
