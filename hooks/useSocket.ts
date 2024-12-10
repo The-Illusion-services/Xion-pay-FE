966bd8c811a5ca205ac16368a8257abeeb41a4bc
preciousaffiah <preciousaffiah205@gmail.com>
2024-12-10 23:33:36 +0100

    updated base url

--- hooks/useSocket.ts
+++ hooks/useSocket.ts
@@ -10,8 +10,8 @@ function useSocket() {
     useContext(ConversationContext);
 
   const socket = useMemo(() => {
-    // return io("https://youchatbackend-kga1.onrender.com", {
-    return io("http://localhost:3001", {
+    return io("https://youchatbackend-kga1.onrender.com", {
+    // return io("http://localhost:3001", {
       query: { token: token?.trim() },
       autoConnect: false, // Disable automatic connection on initialization
     });
